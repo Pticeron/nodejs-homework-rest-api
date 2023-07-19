f983c69ac2aa8b3945c29c29051dbcbebeda4ce4
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-07-19 07:07:25 +0300

    add jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,11 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const { User } = require("../models/user");
 
 const { ctrlWrapper, HttpError } = require("../helpers");
+
+const SECRET_KEY = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -33,11 +36,16 @@ const login = async (req, res) => {
   if (!passwordCompare) {
     throw HttpError(401, "Not authorized");
   }
-  const token = "afgr125dgetr235"
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
 
   res.json({
     token,
-  })
+  });
 };
 
 module.exports = {
